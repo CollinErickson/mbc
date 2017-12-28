dd05abbf194baf6d4f411c4ab856519fcc90edba
Collin Erickson <collinberickson@gmail.com>
2017-12-27 23:21:55 -0600

    Working on autoplot for mbc

--- R/autoplot_mbc.R
+++ R/autoplot_mbc.R
@@ -18,7 +18,7 @@
 #' @examples
 #' library("ggplot2")
 #'
-#' tm <- microbenchmark(rchisq(100, 0),
+#' tm <- mbc(rchisq(100, 0),
 #'                      rchisq(100, 1),
 #'                      rchisq(100, 2),
 #'                      rchisq(100, 3),
@@ -28,6 +28,26 @@
 autoplot.mbc <- function(object, ...,
                                     log=TRUE,
                                     y_max=1.05 * max(object$time)) {
+  # browser()
+  # 
+  # y_min <- 0
+  # # object$ntime <- convert_to_unit(object$time, "t")
+  # melt_object <- reshape2::melt(object, id.vars=c('expr'))
+  # plt <- ggplot(melt_object, ggplot2::aes(x=expr, y=value)) + facet_grid(. ~ variable, scales='free')#ggplot2::aes_string(x="expr", y="ntime"))
+  # plt <- plt + coord_cartesian(ylim=c(y_min , y_max))
+  # plt <- plt + stat_ydensity()
+  # plt <- plt + scale_x_discrete(name="")
+  # plt <- if (log) {
+  #   plt + scale_y_log10(name=sprintf("Time [%s]",
+  #                                    attr(object$ntime, "unit")))
+  # } else {
+  #   plt + scale_y_continuous(name=sprintf("Time [%s]",
+  #                                         attr(object$ntime, "unit")))
+  # }
+  # plt <- plt + ggplot2::coord_flip()
+  # print(plt)
+  
+  
   y_min <- 0
   object$ntime <- convert_to_unit(object$time, "t")
   plt <- ggplot(object, ggplot2::aes_string(x="expr", y="ntime"))
@@ -43,4 +63,27 @@ autoplot.mbc <- function(object, ...,
   }
   plt <- plt + ggplot2::coord_flip()
   plt
+  
+  
+  # autoplot V1
+  
+  y_min <- min(object$V1) - 0.03 * (max(object$V1) - min(object$V1))
+  y_max <- max(object$V1) + 0.03 * (max(object$V1) - min(object$V1))
+  # object$ntime <- convert_to_unit(object$time, "t")
+  plt2 <- ggplot(object, ggplot2::aes_string(x="expr", y="V1"))
+  plt2 <- plt2 + coord_cartesian(ylim=c(y_min , y_max))
+  plt2 <- plt2 + stat_ydensity()
+  plt2 <- plt2 + scale_x_discrete(name="")
+  # plt <- if (log) {
+  #   plt + scale_y_log10(name=sprintf("Time [%s]",
+  #                                    attr(object$ntime, "unit")))
+  # } else {
+  #   plt + scale_y_continuous(name=sprintf("Time [%s]",
+  #                                         attr(object$ntime, "unit")))
+  # }
+  plt2 <- plt2 + scale_y_continuous(name="V1")
+  plt2 <- plt2 + ggplot2::coord_flip()
+  plt2
+  
+  ggpubr::ggarrange(plt, plt2, ncol=2, nrow=1)
 }
